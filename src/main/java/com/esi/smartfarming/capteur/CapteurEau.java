41c2e7af64ca56c41b9cdff253879b88806fca0a
23aneessss <oa_bouziani@esi.dz>
2026-05-15 17:13:10 +0100

    Refactor CapteurEau class to include specific measurement attributes and update relever logic

--- src/main/java/com/esi/smartfarming/capteur/CapteurEau.java
+++ src/main/java/com/esi/smartfarming/capteur/CapteurEau.java
@@ -7,20 +7,36 @@ import com.esi.smartfarming.zone.Zone;
 import java.util.Date;
 
 public class CapteurEau extends CapteurNumerique {
+    private float temperateur;
+    private float oxygene;
+    private float ph;
     private String typeCapture;
 
-    public CapteurEau(String code, Zone zone, double seuilMin, double seuilMax, String unite, String typeCapture) {
+    public CapteurEau(String code, Zone zone, double seuilMin, double seuilMax, String unite,
+                      float temperateur, float oxygene, float ph, String typeCapture) {
         super(code, zone, seuilMin, seuilMax, unite);
+        this.temperateur = temperateur;
+        this.oxygene = oxygene;
+        this.ph = ph;
         this.typeCapture = typeCapture;
     }
 
+    public float getTemperateur() { return temperateur; }
+    public void setTemperateur(float temperateur) { this.temperateur = temperateur; }
+
+    public float getOxygene() { return oxygene; }
+    public void setOxygene(float oxygene) { this.oxygene = oxygene; }
+
+    public float getPh() { return ph; }
+    public void setPh(float ph) { this.ph = ph; }
+
     public String getTypeCapture() { return typeCapture; }
+    public void setTypeCapture(String typeCapture) { this.typeCapture = typeCapture; }
 
     @Override
     public ReleveNumerique envoyerReleve() {
-        double valeur = (seuilMin + seuilMax) / 2.0;
-        NiveauReleve niveau = verifierSeuil(valeur) ? NiveauReleve.NORMAL : NiveauReleve.AVERTISSEMENT;
-        ReleveNumerique releve = new ReleveNumerique(historique.size() + 1, this, new Date(), niveau, valeur, unite, this);
+        NiveauReleve niveau = verifierSeuil(temperateur) ? NiveauReleve.NORMAL : NiveauReleve.AVERTISSEMENT;
+        ReleveNumerique releve = new ReleveNumerique(historique.size() + 1, this, new Date(), niveau, temperateur, unite, this);
         historique.add(releve);
         return releve;
     }
